8ffdf73ad21dca0fae09f34aa98bc6dec7304af6
phong lu <phong_lu@symantec.com>
2019-02-19 18:38:58 -0800

    initial checkin

---
+++ LocalFetch/Sources/LocalFetch.h
@@ -0,0 +1,18 @@
+//
+//  LocalFetch.h
+//  LocalFetch
+//
+//  Created by plu on 2/19/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalFetch.
+FOUNDATION_EXPORT double LocalFetch_VersionNumber;
+
+//! Project version string for LocalFetch.
+FOUNDATION_EXPORT const unsigned char LocalFetch_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFetch_iOS/PublicHeader.h>
+
+
