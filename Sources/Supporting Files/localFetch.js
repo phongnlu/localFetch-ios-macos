dce0eed016d0ee37a7b2c6f742ff4fc09cbe7970
phong lu <phong_lu@symantec.com>
2019-02-25 13:23:19 -0800

    no message

--- Sources/Supporting Files/localFetch.js
+++ Sources/Supporting Files/localFetch.js
@@ -1,8 +1,15 @@
-var existingFetch = window.fetch;
-window.fetch = function(uri, json) {
-    if (uri.toLowerCase().indexOf('local://') !== -1) {
-        return localFetch.load(uri, json);
-    } else {
-        return existingFetch(uri, json);
+(function())
+    var existingFetch = window.fetch;
+    window.fetch = function(uri, json) {
+        if (uri.toLowerCase().indexOf('local://') !== -1) {
+            return localFetchWrapper(uri, json);
+        } else {
+            return existingFetch(uri, json);
+        }
     }
-}
+
+    var localFetchWrapper = function(uri, json) {
+        json.uri = uri;
+        return localFetch.load(json);
+    }
+ })();
